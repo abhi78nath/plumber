1cab60945d196e00cd17c231e93e225acc78dc44
abhishek78nath <abhishek78nath@gmail.com>
2026-01-16 20:30:25 +0530

    feat(ui): dashboard layout and initial navigation

---
+++ components/BreadCrumberHeader.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import React from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+} from "./ui/breadcrumb";
+
+function BreadCrumberHeader() {
+  const pathName = usePathname();
+  const paths = pathName === "/" ? [""] : pathName?.split("/");
+  return (
+    <div className="flex items-center flex-start">
+      <Breadcrumb>
+        <BreadcrumbList>
+          {paths.map((path, index) => {
+            return (
+              <React.Fragment key={index}>
+                <BreadcrumbItem>
+                  <BreadcrumbLink className="capitalize" href={`/${path}`}>
+                    {path === "" ? "home" : path}
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+              </React.Fragment>
+            );
+          })}
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
+}
+
+export default BreadCrumberHeader;
