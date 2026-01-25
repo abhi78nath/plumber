9fab6adb6f7986900b735b4fb7cd6bac6dc8be9b
abhishek78nath <abhishek78nath@gmail.com>
2026-01-25 21:13:06 +0530

    feat: home page with execution metrics

--- app/(dashboard)/(home)/page.tsx
+++ app/(dashboard)/(home)/page.tsx
@@ -1,9 +1,102 @@
-import React from 'react'
+import { Suspense } from 'react';
+import { CirclePlayIcon, CoinsIcon, WaypointsIcon } from 'lucide-react';
 
-function HomePage() {
+import { Skeleton } from '@/components/ui/skeleton';
+import PeriodSelector from '@/app/(dashboard)/(home)/_components/periodSelector';
+
+
+// import CreditUsageChart from '@/app/(dashboard)/billing/_components/credit-usage-chart';
+
+
+
+
+// import { getCreditsUsageInPeriod } from '@/actions/analytics/get-credits-usage-in-period';
+
+import { getTimePeriods } from '@/actions/analytics/getTimePeriods';
+import { Period } from '@/types/analytics';
+import StatsCard from './_components/statsCard';
+import { getStatsCardsValues } from '@/actions/analytics/getStatsCardValues';
+import { getWorkflowExecutionStats } from '@/actions/analytics/getWorkflowExecutionCharts';
+import ExecutionStatusChart from './_components/executionStatusChart';
+
+export default function HomePage({
+    searchParams
+}: {
+    searchParams: {
+        month?: string;
+        year?: string
+    }
+}
+) {
+    const currentDate = new Date();
+    const { month, year } = searchParams;
+
+    const period: Period = {
+        month: month ? parseInt(month) : currentDate.getMonth(),
+        year: year ? parseInt(year) : currentDate.getFullYear(),
+    };
+
     return (
-        <div>HomePage</div>
-    )
+        <div className="flex flex-1 flex-col h-full">
+            <div className="flex justify-between">
+                <h1 className="text-3xl font-bold">Home</h1>
+                <Suspense fallback={<Skeleton className="w-[180px] h-[40px]" />}>
+                    <PeriodSelectorWrapper selectedPeriod={period} />
+                </Suspense>
+            </div>
+            <div className="h-full py-6 flex flex-col gap-4">
+                <Suspense fallback={<StatsCardSkeleton />}>
+                    <StatsCards selectedPeriod={period} />
+                </Suspense>
+                <Suspense fallback={<Skeleton className="w-full h-[300px]" />}>
+                    <StatsExecutionStatus selectedPeriod={period} />
+                </Suspense>
+                {/* <Suspense fallback={<Skeleton className="w-full h-[300px]" />}>
+                    <CreditsUsageInPeriod selectedPeriod={period} />
+                </Suspense> */}
+            </div>
+        </div>
+    );
 }
 
-export default HomePage;
+async function PeriodSelectorWrapper({ selectedPeriod }: { selectedPeriod: Period }) {
+    const periods = await getTimePeriods();
+
+    return <PeriodSelector selectedPeriod={selectedPeriod} periods={periods} />;
+}
+
+async function StatsCards({ selectedPeriod }: { selectedPeriod: Period }) {
+    const data = await getStatsCardsValues(selectedPeriod);
+
+    return (
+        <div className="grid gap-3 lg:gap-8 lg:grid-cols-3 min-h-[120px]">
+            <StatsCard title="Workflow executions" value={data.workflowExecutions} icon={CirclePlayIcon} />
+            <StatsCard title="Phase executions" value={data.phaseExecutions} icon={WaypointsIcon} />
+            <StatsCard title="Credits consumed" value={data.creditsConsumed} icon={CoinsIcon} />
+        </div>
+    );
+}
+
+function StatsCardSkeleton() {
+    return (
+        <div className="grid gap-3 lg:gap-8 lg:grid-cols-3">
+            {[1, 2, 3].map((i) => (
+                <Skeleton key={i} className="w-full min-h-[120px]" />
+            ))}
+        </div>
+    );
+}
+
+async function StatsExecutionStatus({ selectedPeriod }: { selectedPeriod: Period }) {
+    const data = await getWorkflowExecutionStats(selectedPeriod);
+
+    return <ExecutionStatusChart data={data} />;
+}
+
+// async function CreditsUsageInPeriod({ selectedPeriod }: { selectedPeriod: Period }) {
+//     const data = await getCreditsUsageInPeriod(selectedPeriod);
+
+//     return (
+//         <CreditUsageChart data={data} title="Daily credits spent" description="Daily credits consumed in selected period" />
+//     );
+// }
--- lib/helper/dates.ts
+++ lib/helper/dates.ts
@@ -1,3 +1,4 @@
+import { Period } from '@/types/analytics';
 import { endOfMonth, intervalToDuration, startOfMonth } from 'date-fns';
 
 // import { Period } from '@/types/analytics';
@@ -19,9 +20,9 @@ export function datesToDurationString(end: Date | null | undefined, start: Date 
   return `${duration.minutes || 0}m ${duration.seconds || 0}s`;
 }
 
-// export function periodToDateRange(period: Period) {
-//   const startDate = startOfMonth(new Date(period.year, period.month));
-//   const endDate = endOfMonth(new Date(period.year, period.month));
+export function periodToDateRange(period: Period) {
+  const startDate = startOfMonth(new Date(period.year, period.month));
+  const endDate = endOfMonth(new Date(period.year, period.month));
 
-//   return { startDate, endDate };
-// }
+  return { startDate, endDate };
+}
