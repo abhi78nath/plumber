b3710da927050fc0aafba8b651c29c58d81ce95c
abhishek78nath <abhishek78nath@gmail.com>
2026-01-26 01:19:43 +0530

    feat: stripe integration

---
+++ lib/stripe/handleCheckoutSessionCompleted.ts
@@ -0,0 +1,43 @@
+import { getCreditsPack, PackId } from "@/types/billing";
+import "server-only";
+import Stripe from "stripe";
+import prisma from "../prisma";
+
+export async function HandleCheckoutSessionCompleted(
+    event: Stripe.Checkout.Session
+) {
+    if (!event.metadata) {
+        throw new Error("missing metadata");
+    }
+
+    const { userId, packId } = event.metadata;
+
+    if (!userId) {
+        throw new Error("unauthenticated")
+    }
+
+    if (!packId) {
+        throw new Error("missing pack id");
+
+    }
+    const purchasePack = getCreditsPack(packId as PackId);
+
+    if (!purchasePack) {
+        throw new Error("purchased pack not found");
+    }
+
+    await prisma.userBalance.upsert({
+        where: {
+            userId
+        },
+        create: {
+            userId,
+            credits: purchasePack.credits,
+        },
+        update: {
+            credits: {
+                increment: purchasePack.credits
+            }
+        }
+    })
+}
