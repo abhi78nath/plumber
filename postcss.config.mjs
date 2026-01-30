dd6d1e1fd614992aa61f2c491995c9823c91dca0
abhishek78nath <abhishek78nath@gmail.com>
2026-01-31 00:20:02 +0530

    fix: ui

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,13 @@
-export default {
+// export default {
+//   plugins: {
+//     "@tailwindcss/postcss": {},
+//   }
+// }
+/** @type {import('postcss-load-config').Config} */
+const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
-  }
-}
+    tailwindcss: {},
+  },
+};
+
+export default config;
